f310340e566110d9f2a3119ffd7ef820f2a2e82d
Ashair Imran <55155477+ashair12@users.noreply.github.com>
2024-04-25 13:47:30 -0400

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,23 @@
+import boto3
+import json
+from datetime import datetime
+
+print('Loading function')
+dynamodb = boto3.client('dynamodb')
+
+
+
+
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': err.message if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
+
+
+
+def lambda_handler(event, context):
+    dynamodb.put_item(TableName="resume-db", Item={'visitorid':{'S':'LambdaTest'},'timestamp':{'S': datetime.now().isoformat(timespec='seconds')}})
